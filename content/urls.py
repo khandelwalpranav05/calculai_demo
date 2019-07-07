cd3c2303dab09b74211b1fe66b04b54ee904275f
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-07-08 02:11:55 +0530

    detail view updated

--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from content import views
 
+app_name = 'content'
+
 urlpatterns = [
 	path('',views.grid_show,name = 'display_all'),
+	path('<image_id>/detail/',views.detail_show,name = 'detail_image'),
 	]
 	 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- content/views.py
+++ content/views.py
@@ -5,3 +5,8 @@ from .models import ImageContent
 def grid_show(request):
 	images = ImageContent.objects.all()
 	return render(request,"grid_display.html",{"images":images})
+
+def detail_show(request,image_id):
+	image = ImageContent.objects.get(id=image_id)
+	# image = ImageContent.get_object().pk
+	return render(request,'detail_view.html',{'image':image})
