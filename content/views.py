caab7b8fa6415717957c56739e7b8d6b3aeb5935
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-07-08 10:36:13 +0530

    form

--- content/views.py
+++ content/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import ImageContent
 
 def grid_show(request):
-	images = ImageContent.objects.all()
+	if request.method =="POST":
+		a = request.POST['dropdown']
+
+	images = ImageContent.objects.filter(ethnicity = a)
 	return render(request,"grid_display.html",{"images":images})
 
 def detail_show(request,image_id):
@@ -10,9 +13,4 @@ def detail_show(request,image_id):
 	return render(request,'detail_view.html',{'image':image})
 
 def index(request):
-	images = ImageContent.objects.all()
-
-	if request.method =="POST":
-		print("REQUEST***********************")
-
-	return render(request,"index.html",{"images":images})
+	return render(request,"index.html")
